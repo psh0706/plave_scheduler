f8bc027fc15838578187360ee168cc85ca958cce
psh0706 <best8427@naver.com>
2025-01-20 23:59:00 +0900

    project reset & set up project structure

---
+++ components/Calendar/Calendar.tsx
@@ -0,0 +1,68 @@
+import { View, Text } from "react-native";
+
+export function Calendar() {
+  return (
+    <View>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+      <Text>달력 자리</Text>
+    </View>
+  );
+}
